08928eb8d2d195ad218884e8f7c0c33f829735da
Rainer <rainer@websimple.be>
2017-06-24 17:37:59 +0200

    déclaration des champs extras

--- cnde_administrations.php
+++ cnde_administrations.php
@@ -12,7 +12,8 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
-
+include_spip('inc/cextras');
+include_spip('base/cnde');
 
 /**
  * Fonction d'installation et de mise à jour du plugin Squelettes CNDE.
@@ -22,7 +23,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * - créer la structure SQL,
  * - insérer du pre-contenu,
  * - installer des valeurs de configuration,
- * - mettre à jour la structure SQL 
+ * - mettre à jour la structure SQL
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
@@ -32,24 +33,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 **/
 function cnde_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# 
-	# $maj['create'] = array(array('creer_base'));
-	#
-	# include_spip('inc/config')
-	# $maj['create'] = array(
-	#	array('maj_tables', array('spip_xx', 'spip_xx_liens')),
-	#	array('ecrire_config', 'cnde', array('exemple' => "Texte de l'exemple"))
-	#);
-	#
-	# $maj['1.1.0']  = array(array('sql_alter','TABLE spip_xx RENAME TO spip_yy'));
-	# $maj['1.2.0']  = array(array('sql_alter','TABLE spip_xx DROP COLUMN id_auteur'));
-	# $maj['1.3.0']  = array(
-	#	array('sql_alter','TABLE spip_xx CHANGE numero numero int(11) default 0 NOT NULL'),
-	#	array('sql_alter','TABLE spip_xx CHANGE texte petit_texte mediumtext NOT NULL default \'\''),
-	# );
-	# ...
+	cextras_api_upgrade(cnde_declarer_champs_extras(), $maj['create']);
+	cextras_api_upgrade(cnde_declarer_champs_extras(), $maj['1.0.1']);
+
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -58,11 +44,11 @@ function cnde_upgrade($nom_meta_base_version, $version_cible) {
 
 /**
  * Fonction de désinstallation du plugin Squelettes CNDE.
- * 
+ *
  * Vous devez :
  *
  * - nettoyer toutes les données ajoutées par le plugin et son utilisation
- * - supprimer les tables et les champs créés par le plugin. 
+ * - supprimer les tables et les champs créés par le plugin.
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
@@ -74,6 +60,6 @@ function cnde_vider_tables($nom_meta_base_version) {
 	# sql_drop_table('spip_xx');
 	# sql_drop_table('spip_xx_liens');
 
-
+	cextras_api_vider_tables(cnde_declarer_champs_extras());
 	effacer_meta($nom_meta_base_version);
 }
