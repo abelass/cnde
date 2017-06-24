ce0128c2b6cc22f5700382c3937a31b6dd960b61
Rainer <rainer@websimple.be>
2017-06-24 14:43:21 +0200

    import initial

---
+++ cnde_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Squelettes CNDE
+ *
+ * @plugin     Squelettes CNDE
+ * @copyright  2017
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Cnde\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
---
+++ lang/paquet-cnde_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cnde_description' => '',
+	'cnde_nom' => 'Squelettes CNDE',
+	'cnde_slogan' => 'CNDE',
+);
