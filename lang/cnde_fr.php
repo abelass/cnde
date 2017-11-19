913ebac957eb77eeda7626cfa7068397f29819a5
Rainer Müller <rainer@websimple.be>
2017-11-19 16:56:51 +0100

    breadcrumb auteur

--- lang/cnde_fr.php
+++ lang/cnde_fr.php
@@ -6,6 +6,9 @@ if (! defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'acces_membre' => 'Accès membres',
+
 	// C
 	'cnde_titre' => 'Squelettes CNDE',
 
@@ -25,11 +28,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_traduction_souhaite' => 'Traduction souhaitée ?',
 	'lire_suite' => 'Lire la suite',
 
+	// M
+	'mentions_legales' => 'Mentions légales',
+
 	// R
 	'related_links' => 'À  lire également',
 
 	// T
 	'traductions_disponibles' => 'Désolé cette page n\'est pas traduite pour le moment, elle est diponible en : @traductions@.',
+	'travaillez_cnde' => 'Travaillez à la CNDE',
 
 	// V
 	'vous_ici' => 'Vous êtes ici :'
