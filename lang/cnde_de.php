feb874ffa80a21a6e8eddc016c14393c6d86e268
abelass <rainer@websimple.be>
2017-11-30 15:26:16 +0100

    trad allemand, presque rien

---
+++ lang/cnde_de.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file -- Ceci est un fichier langue de SPIP
+if (! defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'acces_membre' => 'Accès membres',
+
+	// C
+	'cnde_titre' => 'Squelettes CNDE',
+	'contactez_nous' => 'Contactez nous',
+
+	// D
+	'decouvrez_cnde' => 'Découvrez la CNDE',
+	'documents_telecharger' => 'Documents à télécharger',
+
+	// F
+	'faits_cles' => 'Les faits clé',
+
+	// I
+	'informations_utiles' => 'Informations utiles',
+
+	// L
+	'label_email' => 'Email :',
+	'label_telephone' => 'Téléphone :',
+	'label_traduction_souhaite' => 'Traduction souhaitée ?',
+	'lire_suite' => 'Lire la suite',
+
+	// M
+	'mentions_legales' => 'Mentions légales',
+
+	// R
+	'related_links' => 'À  lire également',
+
+	// T
+	'traductions_disponibles' => 'Désolé, cette page n\’est actuellement pas disponible en français, nous vous invitons à la consulter en : @traductions@.',
+	'travaillez_cnde' => 'Arbeiten in der NKRK',
+
+	// V
+	'vous_ici' => 'Vous êtes ici :'
+);
